fcd3ae4946a889efdea90af2f82aea3c6a43a1b1
kjhogan <kevinjohnhogan@gmail.com>
2017-04-10 12:39:46 -0400

    Updated player tracking + csv

---
+++ BBRefScraper.R
@@ -0,0 +1,112 @@
+getSeasonStats <- function(year, type) {
+  library(dplyr)
+  library(rvest)
+  library(stringr)
+  library(ggplot2)
+  library(scales)
+  library(grid)
+  library(RColorBrewer)
+  
+  season <- paste0(as.character(as.numeric(year)-1),'-',str_sub(year,-2))
+  url <- paste0('http://www.basketball-reference.com/leagues/NBA_',year,'_',type,'.html')
+  seasonstats <- read_html(url) %>% html_nodes('.stats_table') %>% html_table() %>% data.frame() %>% filter(Rk != 'Rk')
+  seasonstats$Season <- season
+  seasonstats %>% mutate_at(vars(Age,G:PTS),funs(as.numeric)) -> seasonstats
+  return(seasonstats)
+}
+
+getSeasonStatsBetween <- function (startyear, endyear, ssntype) {
+  seasons <- seq(as.numeric(startyear), as.numeric(endyear), by=1)
+  allseasons <- data.frame()
+  for(i in 1:length(seasons)){
+    currentseason <- getSeasonStats(seasons[i], ssntype)
+    print(paste0("Getting data for the ", seasons[i], " season."))
+    allseasons <- rbind(allseasons, currentseason)
+  }
+  return(allseasons)
+}
+
+fte_theme <- function() {
+  
+  # Generate the colors for the chart procedurally with RColorBrewer
+  palette <- brewer.pal("Greys", n=9)
+  color.background = palette[2]
+  color.grid.major = palette[3]
+  color.axis.text = palette[6]
+  color.axis.title = palette[7]
+  color.title = palette[9]
+  
+  # Begin construction of chart
+  theme_bw(base_size=9) +
+    
+    # Set the entire chart region to a light gray color
+    theme(panel.background=element_rect(fill=color.background, color=color.background)) +
+    theme(plot.background=element_rect(fill=color.background, color=color.background)) +
+    theme(panel.border=element_rect(color=color.background)) +
+    
+    # Format the grid
+    theme(panel.grid.major=element_line(color=color.grid.major,size=.25)) +
+    theme(panel.grid.minor=element_blank()) +
+    theme(axis.ticks=element_blank()) +
+    
+    # Format the legend, but hide by default
+    theme(legend.position="none") +
+    theme(legend.background = element_rect(fill=color.background)) +
+    theme(legend.text = element_text(size=7,color=color.axis.title)) +
+    
+    # Set title and axis labels, and format these and tick marks
+    theme(plot.title=element_text(color=color.title, size=14, face = "bold", vjust=1.25)) +
+    theme(axis.text.x=element_text(size=7,color=color.axis.text)) +
+    theme(axis.text.y=element_text(size=7,color=color.axis.text)) +
+    theme(axis.title.x=element_text(size=8,color=color.axis.title, vjust=0)) +
+    theme(axis.title.y=element_text(size=8,color=color.axis.title, vjust=1.25)) +
+    
+    # Plot margins
+    theme(plot.margin = unit(c(0.35, 0.2, 0.3, 0.35), "cm"))
+}
+
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
--- getPlayerTracking.R
+++ getPlayerTracking.R
@@ -1,12 +1,6 @@
-addressList <- list(pullup_address = "http://stats.nba.com/js/data/sportvu/pullUpShootData.js", 
-                    drives_address = "http://stats.nba.com/js/data/sportvu/drivesData.js", defense_address = "http://stats.nba.com/js/data/sportvu/defenseData.js", 
-                    passing_address = "http://stats.nba.com/js/data/sportvu/passingData.js", 
-                    touches_address = "http://stats.nba.com/js/data/sportvu/touchesData.js", 
-                    speed_address = "http://stats.nba.com/js/data/sportvu/speedData.js", rebounding_address = "http://stats.nba.com/js/data/sportvu/reboundingData.js", 
-                    catchshoot_address = "http://stats.nba.com/js/data/sportvu/catchShootData.js", 
-                    shooting_address = "http://stats.nba.com/js/data/sportvu/shootingData.js")
 
-readIt <- function(address) {
+
+getTrackingData <- function(address) {
   web_page <- readLines(address)
   
   ## regex to strip javascript bits and convert raw to csv format
@@ -20,5 +14,76 @@ readIt <- function(address) {
   return(nba)
 }
 
-df_list <- lapply(addressList, readIt)
+updateTrackingData <- function() {
+  library(tidyverse)
+  addressList <- list(pullup_address = "http://stats.nba.com/js/data/sportvu/pullUpShootData.js", 
+                      drives_address = "http://stats.nba.com/js/data/sportvu/drivesData.js", defense_address = "http://stats.nba.com/js/data/sportvu/defenseData.js", 
+                      passing_address = "http://stats.nba.com/js/data/sportvu/passingData.js", 
+                      touches_address = "http://stats.nba.com/js/data/sportvu/touchesData.js", 
+                      speed_address = "http://stats.nba.com/js/data/sportvu/speedData.js", rebounding_address = "http://stats.nba.com/js/data/sportvu/reboundingData.js", 
+                      catchshoot_address = "http://stats.nba.com/js/data/sportvu/catchShootData.js", 
+                      shooting_address = "http://stats.nba.com/js/data/sportvu/shootingData.js")
+  df_list <- lapply(addressList, getTrackingData)
+    write_csv(df_list[[1]], 'data/pullups.csv')
+    write_csv(df_list[[2]], 'data/drives.csv')
+    write_csv(df_list[[3]], 'data/defense.csv')
+    write_csv(df_list[[4]], 'data/passing.csv')
+    write_csv(df_list[[5]], 'data/touches.csv')
+    write_csv(df_list[[6]], 'data/speed.csv')
+    write_csv(df_list[[7]], 'data/rebounding.csv')
+    write_csv(df_list[[8]], 'data/catchshoot.csv')
+    write_csv(df_list[[9]], 'data/shooting.csv')
+    return(df_list)
+}
 
+GetTeamHustleStats <- function(year, season.type, per.mode, quarter) {
+  
+  request <- GET(
+    "http://stats.nba.com/stats/leaguehustlestatsteam",
+    query = list(
+      College = "",
+      Conference = "",
+      Country = "",
+      DateFrom = "",
+      DateTo = "",
+      Division = "",
+      DraftPick = "",
+      GameScope = "",
+      GameSegment = "",
+      Height = "",
+      LastNGames = 0,
+      LeagueID = "00",
+      Location = "",
+      Month = 0,
+      OpponentTeamID = 0,
+      Outcome = "",
+      PORound = 0,
+      PaceAdjust = 'N',
+      PerMode = CleanParam(per.mode),
+      Period = quarter,
+      PlayerExperience = "",
+      PlayerPosition = "",
+      PlusMinus = "N",
+      Rank = "N",
+      Season = YearToSeason(year),
+      SeasonSegment = "",
+      SeasonType = season.type,
+      ShotClockRange = "",
+      TeamID = 0,
+      VsConference = "",
+      VsDivision = "",
+      Weight = ""
+    ),
+    add_headers('User-Agent' = 'Mozilla/5.0')
+  )
+  
+  content <- content(request, 'parsed')[[3]][[1]]
+  stats <- ContentToDF(content)
+  
+  # Clean data frame
+  char.cols <- which(colnames(stats) %in% CHARACTER.COLUMNS)
+  stats[, -char.cols] <- sapply(stats[, -char.cols], as.numeric)
+  
+  return(stats)
+  
+}
