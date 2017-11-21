9a44c005ac7a479d9038fbc4e08029e2d3e7458a
kjhogan <kevinjohnhogan@gmail.com>
2017-11-21 16:56:14 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,22 +1,20 @@
 library(shiny)
-
-dataf <- read_csv("NBADFS-master/data/gamelogs.csv")
+library(DT)
+game_logs <- read_csv("NBADFS-master/data/gamelogs.csv")
 
 
 ui <- fluidPage(
   titlePanel("Player Comparison"),
   sidebarLayout(
-    sidebarPanel(
-      selectizeInput("posInput", "Position",
-                     choices = unique(dataf$Pos),
-                     selected = "PG"),
-      sliderInput("mpInput", "Minutes Played", 0, maxMP, c(1000, maxMP)),
+    sidebarPanel(selectizeInput("statInput", "Stats",
+                                c('PTS'),choices = c('PTS', 'AST', 'REB', 'FGA', 'STL', 'TOV', 'MIN'), multiple = FALSE),
+
       selectizeInput("playerInput", "Players",
-                  c('Kyrie Irving', 'Damian Lillard', 'Isaiah Thomas', 'Kemba Walker'),choices = unique(dataf$Player), multiple = TRUE)
+                  c('Kyrie Irving', 'Damian Lillard', 'Isaiah Thomas', 'Kemba Walker'),choices = unique(game_logs$NAME.PLAYER), multiple = TRUE)
     ),
     mainPanel(
-      plotOutput("testplot", height = "800px"),
-      br(), br()
+      plotOutput("testplot", height = "500px"),
+      br(), br(), DT::dataTableOutput("mytable")
     )
   )
 )
@@ -24,39 +22,28 @@ ui <- fluidPage(
 server <- function(input,output) {
   
   output$testplot <- renderPlot({
-    dataf %>% filter(MP >= input$mpInput[1], MP <= input$mpInput[2], Pos == input$posInput) %>% 
-      mutate(Age = round((Age - mean(Age))/sd(Age), 2)*-1, MP = round((MP - mean(MP))/sd(MP), 2),
-             FGA = round((FGA - mean(FGA))/sd(FGA), 2),
-             FTA = round((FTA - mean(FTA))/sd(FTA), 2),
-             x3PA = round((X3PA - mean(X3PA))/sd(X3PA), 2),  
-             AST = round((AST - mean(AST))/sd(AST), 2),
-             STL = round((STL - mean(STL))/sd(STL), 2),
-             TOV = round((TOV - mean(TOV))/sd(TOV), 2)) -> dataf
+ 
     
-    ranks <- dataf %>% filter(Player %in% input$playerInput) %>% 
-      select(Player, Pos, Tm, Age, MP, FGA, FTA, x3PA, AST, STL, TOV) %>% 
-      gather(Age:STL, key = "Stat", value = "Performance")
+    game_logs <- game_logs %>% filter(NAME.PLAYER %in% input$playerInput)
     
-    ranks$Stat <- factor(ranks$Stat, levels = c("Age", "MP", "FGA", "x3PA", "FTA", "AST", "STL"))
-    ranks %>% ggplot(aes(x=Stat, y=Performance, fill = Player)) + 
-      geom_point(stat='identity', aes(col=Player), size = 3)  +
-      geom_segment(aes(y = 0, 
-                       x = Stat, 
-                       yend = Performance, 
-                       xend = Stat, color = Player), size = 2 ) +
-      scale_x_discrete(limits = rev(levels(ranks$Stat))) +
-      scale_y_continuous(breaks = c(-1,0,1), label = c("Poor", "Average", "Great")) +
-      facet_wrap(~Player, ncol=2) +
-      labs(x="Stat", y="Performance",
-           title="Player Comparison",
-           subtitle="Performance vs. average player for various statistics",
-           caption="data from BBRef") + 
-      theme_ipsum() + 
-      theme(legend.position = "none", strip.text = element_text(face = "bold", size = 11), panel.grid.major.y = element_blank()) + 
-      scale_color_viridis(discrete = TRUE) +
-      coord_flip()
+    game_logs %>% ggplot(aes_string(x=input$statInput, y = paste0("reorder(NAME.PLAYER,",input$statInput,",median)"))) +
+      geom_density_ridges(fill = "skyblue4",alpha = .8, color = "black") +
+      scale_y_discrete(expand = c(.01, 0)) +
+      scale_x_continuous(expand = c(.01, 0)) +
+      labs(title = paste0("Player Comparison: ", input$statInput)) +
+      theme_joy(font_size = 13, grid = TRUE) +
+      theme (axis.title.y = element_blank(), legend.position = "none") 
   })
   
+  output$mytable = DT::renderDataTable({
+    
+    game_logs <- game_logs %>% filter(NAME.PLAYER %in% input$playerInput)
+    variance <- game_logs %>% group_by(NAME.PLAYER) %>% summarise_(Avg = interp(~mean(x), x = as.name(input$statInput)),
+                                                                   Median = interp(~median(x), x = as.name(input$statInput)),
+                                                                   Variance = interp(~var(x), x = as.name(input$statInput)),
+                                                                   StandardDev = interp(~sd(x), x = as.name(input$statInput)))
+    variance
+  })
 }
 
 
