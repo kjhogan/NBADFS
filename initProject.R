f2b4e22379f07bf0a70a0b215151ab522a0f9a86
kjhogan <kevinjohnhogan@gmail.com>
2021-01-31 16:15:10 -0500

    Add files via upload

---
+++ initProject.R
@@ -0,0 +1,7 @@
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(rvest))
+suppressPackageStartupMessages(library(nbastatR))
+suppressPackageStartupMessages(library(gambleR))
+suppressPackageStartupMessages(library(httr))
+suppressPackageStartupMessages(library(janitor))
+suppressPackageStartupMessages(library(jsonlite))
